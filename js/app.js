f9aa4d87140a5a16edd637a73aa07cec94682e13
Valeria <valeriacabrera45x@gmail.com>
2024-10-01 21:51:45 +0200

    Added Grid and changed colors

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('navbarToggle').addEventListener('click', function() {
+    const links = document.querySelector('.navbar-links');
+    links.classList.toggle('active');
+});
