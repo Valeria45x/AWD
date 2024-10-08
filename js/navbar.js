5595977a026077bf4c84fad9dce223aebdf1e5e7
Valeria <valeriacabrera45x@gmail.com>
2024-10-08 20:41:40 +0200

    made navbar in css

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-// Solo una vez en el archivo
-const navbarToggle = document.getElementById('navbarToggle');
-const links = document.querySelector('.navbar-links');
-
-// Añadir un evento 'click' al botón de hamburguesa
-navbarToggle.addEventListener('click', function() {
-    const isActive = links.classList.toggle('active');
-    navbarToggle.setAttribute('aria-expanded', isActive);
-});
